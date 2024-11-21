a31c6101b216cd204f193b9879e8b1dc9c5d215e
Anas Chebili <anaschebili@gmail.com>
2024-11-21 19:12:20 +0100

    phased out of raw sql in favor of prisma operations

--- src/app/routes/products/products.ts
+++ src/app/routes/products/products.ts
@@ -23,18 +23,14 @@ export default async function (fastify: FastifyInstance) {
       }>,
       reply: FastifyReply
     ) {
-      const { id } = request.params;
-      const client = await fastify.pg.connect();
+      const id = Number(request.params.id);
       try {
-        const { rows } = await client.query(
-          'SELECT * FROM products WHERE id = $1',
-          [id]
-        );
+        const rows = await fastify.prisma.products.findUnique({
+          where: { id: id },
+        });
         return rows;
       } catch (error) {
         console.log(error);
-      } finally {
-        client.release();
       }
     }
   );
@@ -47,19 +43,13 @@ export default async function (fastify: FastifyInstance) {
       }>,
       reply: FastifyReply
     ) {
-      const { title, description, price, brand, category, thumbnail } =
-        request.body;
-      const client = await fastify.pg.connect();
       try {
-        const response = await client.query(
-          'INSERT INTO products (title, description, price, brand, category, thumbnail) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id',
-          [title, description, price, brand, category, thumbnail]
-        );
+        const response = await fastify.prisma.products.create({
+          data: request.body,
+        });
         return response;
       } catch (error) {
         console.log(error);
-      } finally {
-        client.release();
       }
     }
   );
@@ -73,20 +63,15 @@ export default async function (fastify: FastifyInstance) {
       }>,
       reply: FastifyReply
     ) {
-      const { id } = request.params;
-      const { title, description, price, brand, category, thumbnail } =
-        request.body;
-      const client = await fastify.pg.connect();
+      const id = Number(request.params.id);
       try {
-        const response = await client.query(
-          'UPDATE products SET title = $1, description = $2, price = $3, brand = $4, category = $5, thumbnail = $6 WHERE id = $7',
-          [title, description, price, brand, category, thumbnail, id]
-        );
+        const response = await fastify.prisma.products.update({
+          where: { id },
+          data: request.body,
+        });
         return response;
       } catch (error) {
         console.log(error);
-      } finally {
-        client.release();
       }
     }
   );
@@ -99,18 +84,14 @@ export default async function (fastify: FastifyInstance) {
       }>,
       reply: FastifyReply
     ) {
-      const { id } = request.params;
-      const client = await fastify.pg.connect();
+      const id = Number(request.params.id);
       try {
-        const response = await client.query(
-          'DELETE FROM products WHERE id = $1',
-          [id]
-        );
+        const response = await fastify.prisma.products.delete({
+          where: { id },
+        });
         return response;
       } catch (error) {
         console.log(error);
-      } finally {
-        client.release();
       }
     }
   );
