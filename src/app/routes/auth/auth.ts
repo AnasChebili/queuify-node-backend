5c41d1031b219de5faa222b9e2aa232a03a116fd
Anas Chebili <anaschebili@gmail.com>
2024-12-06 11:18:53 +0100

    created first draft for login route

--- src/app/routes/auth/auth.ts
+++ src/app/routes/auth/auth.ts
@@ -1,24 +1,36 @@
 import { FastifyInstance } from 'fastify';
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { HttpError } from '../../../errors/http-error';
+import { UserRequestSchema } from '../../../schemas/auth-schema';
+
+const bcrypt = require('bcrypt');
 
 export default async function (fastify: FastifyInstance) {
-  fastify.post('/login', async (request, reply) => {
-    const { username, password } = request.body;
-
-    // Find the user
-    const user = users.find(
-      (u) => u.username === username && u.password === password
+  fastify
+    .withTypeProvider<ZodTypeProvider>()
+    .post(
+      'login',
+      { schema: { body: UserRequestSchema } },
+      async function (request, response) {
+        const { email, password } = request.body;
+        const user = await fastify.prisma.user.findFirst({
+          where: { email: email },
+        });
+        if (!user || !user.passwordHash) {
+          throw new HttpError('invalid email or password', 403);
+        }
+        const isPasswordValid = await bcrypt.compare(
+          password,
+          user.passwordHash
+        );
+        if (!isPasswordValid) {
+          throw new HttpError('invalid email or password', 403);
+        }
+        const token = fastify.jwt.sign({
+          email,
+          passwordhash: user.passwordHash,
+        });
+        return token;
+      }
     );
-
-    if (!user) {
-      return reply.status(401).send({ error: 'Invalid credentials' });
-    }
-
-    // Generate JWT
-    const token = fastify.jwt.sign(
-      { id: user.id, username: user.username },
-      { expiresIn: '1h' }
-    );
-
-    return reply.send({ token });
-  });
 }
