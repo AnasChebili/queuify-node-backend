1ec8673326b645b222cf3cb3b613de2646235a59
Anas Chebili <anaschebili@gmail.com>
2024-12-07 18:15:13 +0100

    created first draft for register route

--- src/app/routes/auth/auth.ts
+++ src/app/routes/auth/auth.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify';
 import { ZodTypeProvider } from 'fastify-type-provider-zod';
 import { HttpError } from '../../../errors/http-error';
 import { UserRequestSchema } from '../../../schemas/auth-schema';
+import { ValidationError } from '../../../errors/validation-error';
 
 const bcrypt = require('bcrypt');
 
@@ -9,7 +10,7 @@ export default async function (fastify: FastifyInstance) {
   fastify
     .withTypeProvider<ZodTypeProvider>()
     .post(
-      'login',
+      '/login',
       { schema: { body: UserRequestSchema } },
       async function (request, response) {
         const { email, password } = request.body;
@@ -31,6 +32,33 @@ export default async function (fastify: FastifyInstance) {
           passwordhash: user.passwordHash,
         });
         return token;
+      }
+    );
+
+  fastify
+    .withTypeProvider<ZodTypeProvider>()
+    .post(
+      '/register',
+      { schema: { body: UserRequestSchema } },
+      async function (request, reply) {
+        const { email, password } = request.body;
+        const isUser = await fastify.prisma.user.findFirst({
+          where: { email: email },
+        });
+        if (isUser) {
+          throw new ValidationError('email already in use', {
+            email: 'email already exists',
+          });
+        }
+        const saltRounds = 10;
+        const passwordHash = await bcrypt.hash(password, saltRounds);
+        const user = await fastify.prisma.user.create({
+          data: { email: email, passwordHash: passwordHash },
+        });
+        const token = await fastify.jwt.sign({
+          email,
+          passwordHash: passwordHash,
+        });
       }
     );
 }
