149e9994bc9e29fa19835079661d8f651450fcbc
Anas Chebili <anaschebili@gmail.com>
2024-12-28 12:39:41 +0100

    implemented http route to get websocket status and websocket route for chatting functionality

--- src/app/routes/websocket/websocket.ts
+++ src/app/routes/websocket/websocket.ts
@@ -1,8 +1,57 @@
+import { WebSocket as WebSocketType } from '@fastify/websocket';
 import { FastifyInstance } from 'fastify';
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { Connection } from 'pg';
+import { ChatMessageSchema } from '../../../schemas/chat-schema';
+import { z } from 'zod';
+import { timeStamp } from 'console';
+import { WebSocket } from 'ws';
+
+const connectedClients = new Set<WebSocketType>();
+
 export default async function (fastify: FastifyInstance) {
-  fastify.get('/', { websocket: true }, (socket, request) => {
+  fastify.withTypeProvider<ZodTypeProvider>().get(
+    '/ws-status',
+    {
+      schema: {
+        response: {
+          200: z.object({
+            connections: z.number(),
+            status: z.literal('active'),
+          }),
+        },
+      },
+    },
+    async () => {
+      return {
+        connections: connectedClients.size,
+        status: 'active' as const,
+      };
+    }
+  );
+  fastify.get('/chat', { websocket: true }, (socket, request) => {
+    connectedClients.add(socket);
+    const welcomeMessage = {
+      type: 'join',
+      username: 'System',
+      content: 'Welcome to the chat!',
+      timestamp: new Date().toISOString(),
+    };
+    socket.send(JSON.stringify(ChatMessageSchema.parse(welcomeMessage)));
     socket.on('message', (message) => {
-      socket.send('hi from server');
+      const data = ChatMessageSchema.parse(message);
+      const chatMessage = {
+        type: 'message',
+        username: data.username,
+        content: data.content,
+        timeStamp: new Date().toISOString(),
+      };
+
+      connectedClients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(ChatMessageSchema.parse(chatMessage)));
+        }
+      });
     });
   });
 }
